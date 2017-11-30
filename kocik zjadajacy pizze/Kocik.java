a94993db682d46d55fbd8a134d29ffa52c9ebdc3
Nuxusek <Nuxusek@gihub.pl>
2017-11-30 11:29:30 +0100

    Revert "Merge branch 'master' of https://github.com/Nuxus12/Prace-z-lekcji"
    This reverts commit c5c751cbf8e2d277e44b6e79bce8d5771d19a5f8, reversing
    changes made to 8b4c839680425de4df856d5429c6779e9ee16625.

--- kocik zjadajacy pizze/Kocik.java
+++ kocik zjadajacy pizze/Kocik.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Kocik extends Actor
 {
+    int punkty = 0;
+    boolean tanczylem = true ;
     public void ruch ()
     {
         if (Greenfoot.isKeyDown ("right"))
@@ -18,18 +20,49 @@ public class Kocik extends Actor
         {
             move(-6);
         }
+
     }
 
     public void zjadaniepizzy ()
     {
-        if( isTouching (Pizza.class))removeTouching(Pizza.class);
+        if( isTouching (Pizza.class))
+        {
+            removeTouching(Pizza.class);
+            punkty ++ ;
+            tanczylem = false;
+        }
+    }
+
+    public void wyswietlPunkty ()
+    {
+        World swiat=this.getWorld () ;
+        swiat.showText ("Pizze: " + punkty , 70,40 ) ;
         
+
+    }
+
+    public void tanczenie()
+    {
+        if ( punkty % 5 == 0 && !tanczylem )
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                this.setImage("cat-dance.png");
+                Greenfoot.delay (20);
+                this.setImage("cat-dance-2.png");
+                Greenfoot.delay (20);
+            }
+            this.setImage("cat-sit.png");
+            tanczylem =true ;
+        }
     }
 
     public void act () 
     {
         ruch ();
         zjadaniepizzy();
+        wyswietlPunkty ();
+        tanczenie () ;
     }    
 }
 
