970d786008dd653d9d9b6fe51a9704c623e83a38
Nuxusek <Nuxusek@gihub.pl>
2017-11-30 14:03:57 +0100

    prace z czwartku

--- zaddom/Hipcio.java
+++ zaddom/Hipcio.java
@@ -11,21 +11,24 @@ public class Hipcio extends Actor
 {
     int krok=(3) ;
     int losuj = Greenfoot.getRandomNumber(160);
-    int szybkosc=Greenfoot.getRandomNumber(3);
-    
-    
+    int szybkosc=Greenfoot.getRandomNumber(3)+1;
+
     public void Odbijanie ()
     {
         int losuj = Greenfoot.getRandomNumber(100);
-        
-        if (isAtEdge())krok=krok * (szybkosc);
-        if (isAtEdge() )turn(losuj);
-   
+
+        if (isAtEdge())krok=Greenfoot.getRandomNumber(10)+1;
+
+        if ( isAtEdge())  turn(losuj);
+
+
     }
-     public void Ruch ()
-  {
-      move(krok);
+
+    public void Ruch ()
+    {
+        move(krok);
     }
+
     public void act() 
     {
         Ruch ();
