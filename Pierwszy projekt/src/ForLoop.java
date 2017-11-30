8b4c839680425de4df856d5429c6779e9ee16625
Nuxusek <Nuxusek@gihub.pl>
2017-11-30 11:27:45 +0100

    k
    k

---
+++ Pierwszy projekt/src/ForLoop.java
@@ -0,0 +1,24 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Programik z pêtlami");
+		for (int i = 0; i < 10 ; i ++)
+		{
+			System.out.print(i + " ");
+		}
+		System.out.println("");
+		for (int i = 9; i >= 0 ; i --)
+		{
+			System.out.print(i + " ");
+		}
+		System.out.println("");
+		for (int i = 0; i < i*2 ; i ++)
+		{
+			System.out.print(i + " ");
+		}
+	}
+
+}
